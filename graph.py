2fdc8d4d77f5efbfa207402a2d9a478d14009dcd
paulduchesne <paulduchesne@tuta.io>
2025-10-28 09:18:30 +1100

    class test

--- graph.py
+++ graph.py
@@ -13,13 +13,20 @@ def append_ttl(path):
     return rdflib.Graph().parse(ttl_path)
 
 graph = rdflib.Graph()
-
 for p in [
     'fiafcore/fiafcore',
     'fiafcore-bfi/fiafcore_bfi',
     'fiafcore-bundesarchiv/fiafcore_bundesarchiv'
     ]:
     graph += append_ttl(p)
+
+
+# for now we require some manual labelling.
+
+# graph += 
+
+graph.add((rdflib.URIRef('http://www.w3.org/2002/07/owl#Class'), rdflib.RDFS.label, rdflib.Literal("Class")))
+
 
 graph.serialize(
     destination=pathlib.Path.cwd() / "graph.ttl",
--- ontology/app.py
+++ ontology/app.py
@@ -4,40 +4,80 @@ from flask import Flask
 from flask import render_template
 
 
+
+def entity_label(graph, entity):
+    ''' Return an entity URI and label. '''
+
+    label = [o for s,p,o in graph.triples((entity, rdflib.RDFS.label, None))]
+    if not len(label):
+        raise Exception('label not found.')
+
+    return {'uri':entity, 'label':label[0]}
+
+
 app = Flask(__name__)
 
 @app.route('/<name>')
 def entity(name):
 
-    # mimic sparql call here, if no response send to 404, otherwise render payload on the page
+    # mimic sparql call here, if no response send to 404, otherwise render payload on the page.
 
     graph_path = pathlib.Path.cwd().parent / 'graph.ttl'
     if not graph_path.exists():
         raise Exception('Graph doc not found.')
-
 
     test_uri = 'https://ontology.fiafcore.org/Work'
 
     # query payload and return result, then make dynamic
 
     g = rdflib.Graph().parse(graph_path)
-
     triples = [(s,p,o) for s,p,o in g.triples((rdflib.URIRef(test_uri), None, None))]
-
     if not len(triples):
         return render_template('missing.html')
-
-        # if triples returned are zero, return a 404 page,
-
-
     else:
 
+        # detect a type
 
-        # otherwise detect a type
+        entity_type = [x for x in triples if x[1] == rdflib.RDF.type][0][2]
+        if entity_type == rdflib.URIRef('http://www.w3.org/2002/07/owl#Class'):
+            print('CLASS')
 
-        entity_type = [x for x in triples if x[1] == rdflib.RDF.type][0][2] # returns "class"
+            data = {
+                'entity': entity_label(g, rdflib.URIRef(test_uri)),
+                'type': entity_label(g, entity_type)
+
+                
+                }
+
+
+
+
+
+            # for x in triples:
+            #     print(x)
+            return render_template('class.html', data=data)
 
         # IF ENTITY_TYPE IS CLASS< render class.html
+
+        # http://www.w3.org/2002/07/owl#Class
+
+        # do we want to run jinja, or python
+        # lets try jinja and see how far we get.
+
+        # data = {
+        #     'label':'hello'
+        # }
+
+
+        # http://www.w3.org/2002/07/owl#Class
+
+
+        print(entity_type)
+
+        print(type(entity_type))
+
+
+
 
         # and render the page, dependant on type - type should probably resolve to a template
 
